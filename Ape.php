ff7970b1a3d4e88ad2e80cdaab341eecc63c83d6
TopanKurniawan1 <topankurniawan41@gmail.com>
2025-01-07 19:30:24 +0700

    Update Ape.php

--- Ape.php
+++ Ape.php
@@ -4,7 +4,7 @@ require_once "Animal.php";
 class Ape extends Animal {
     public function __construct($name) {
         parent::__construct($name);
-        $this->legs = 2; // Kera memiliki 2 kaki
+        $this->legs = 2; // kera memiliki 2 kaki
     }
 
     public function yell() {
