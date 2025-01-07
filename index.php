37ed9fdb579a5a02f3e32a8bba5944cd491e1a96
TopanKurniawan1 <topankurniawan41@gmail.com>
2025-01-07 18:02:12 +0700

    task-oop

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+require_once "Animal.php";
+require_once "Frog.php";
+require_once "Ape.php";
+
+echo "<pre>"; // Opsional: untuk menjaga format teks jika dibutuhkan
+
+// Instance untuk Animal
+$sheep = new Animal("shaun");
+echo "Name: " . $sheep->get_name() . "<br>";
+echo "Legs: " . $sheep->get_legs() . "<br>";
+echo "Cold Blooded: " . $sheep->get_cold_blooded() . "<br><br>";
+
+// Instance untuk Frog
+$kodok = new Frog("buduk");
+echo "Name: " . $kodok->get_name() . "<br>";
+echo "Legs: " . $kodok->get_legs() . "<br>";
+echo "Cold Blooded: " . $kodok->get_cold_blooded() . "<br>";
+echo "Jump: ";
+$kodok->jump();
+echo "<br><br>";
+
+// Instance untuk Ape
+$sungokong = new Ape("kera sakti");
+echo "Name: " . $sungokong->get_name() . "<br>";
+echo "Legs: " . $sungokong->get_legs() . "<br>";
+echo "Cold Blooded: " . $sungokong->get_cold_blooded() . "<br>";
+echo "Yell: ";
+$sungokong->yell();
+echo "</pre>";
+?>
